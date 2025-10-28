42d3951de905c759a0a6b97ce2fc68efbf3b3653
maxtong2023 <maxtong2027@u.northwestern.edu>
2025-10-28 11:32:02 -0500

    added some initial thoughts

--- elevator.java
+++ elevator.java
@@ -1,5 +1,38 @@
+import java.util.Queue;
+
 public class elevator {
-    
+    private int currentFloor; 
+    private int highestFloor; 
+    private int lowestFloor; 
+    private int maxElevatorWeight; 
+    private Queue<Integer> floorQueue;
+
+    public elevator(int currentFloor, int highestFloor, int lowestFloor, int maxElevatorWeight) {
+        this.currentFloor = currentFloor;
+        this.highestFloor = highestFloor;
+        this.lowestFloor = lowestFloor;
+        this.maxElevatorWeight = maxElevatorWeight;
+        this.floorQueue = new LinkedList<>();
+    }
+
+    // So I never thought about how an elevator actually works, but I think it works something like a queue. 
+
+    // second thought, let's say that a more efficient pathing exists. Let's say that one person is on the top 
+    // floor and wants to go to the bottom floor. While they are traveling, another person on the 3rd floor wants to 
+    // go down, and after them a person on the 5 floor wants to also go down. We should pick up the 5th floor guy first
+    // this sounds more like a priority queue. 
+
+
+    public void setFloor(int floor) {
+
+    }
+
+    public int getFloor(){
+        return currentFloor;
+    }
+
+
+
 }
 
 
