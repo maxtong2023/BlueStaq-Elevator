aff45f2f373fc6d05d4ad3b493ea95317a38be22
maxtong2023 <maxtong2027@u.northwestern.edu>
2025-10-29 10:22:46 -0500

    made some progress

--- elevator.java
+++ elevator.java
@@ -1,18 +1,28 @@
-import java.util.Queue;
+import java.util.List;
+import java.util.ArrayList;
 
 public class elevator {
     private int currentFloor; 
     private int highestFloor; 
     private int lowestFloor; 
     private int maxElevatorWeight; 
-    private Queue<Integer> floorQueue;
+    private List<Integer> floorQueue;
+    private enum Direction {
+        UP,
+        DOWN,
+        IDLE
+    }
+    private Direction direction;
+    
+
 
     public elevator(int currentFloor, int highestFloor, int lowestFloor, int maxElevatorWeight) {
         this.currentFloor = currentFloor;
         this.highestFloor = highestFloor;
         this.lowestFloor = lowestFloor;
         this.maxElevatorWeight = maxElevatorWeight;
-        this.floorQueue = new LinkedList<>();
+        this.floorQueue = new ArrayList<>();
+        this.direction = Direction.IDLE;
     }
 
     // So I never thought about how an elevator actually works, but I think it works something like a queue. 
@@ -21,6 +31,12 @@ public class elevator {
     // floor and wants to go to the bottom floor. While they are traveling, another person on the 3rd floor wants to 
     // go down, and after them a person on the 5 floor wants to also go down. We should pick up the 5th floor guy first
     // this sounds more like a priority queue. 
+
+    // Let me think about how elevators work. I've been in a lot of them.
+
+    // nah its not a priority queue. That doesn't make sense. reversing directions would make the priority
+    // flip, which is a pain. I think it would be better to keep it simple and use a list.
+
 
 
     public void setFloor(int floor) {
@@ -31,9 +47,47 @@ public class elevator {
         return currentFloor;
     }
 
+    public void requestFloor(int start, int destination){
+        if(destination > highestFloor || destination < lowestFloor){
+            throw new IllegalArgumentException("Pick a valid destination");
+
+        }
+        if(start > highestFloor || start < lowestFloor){
+            throw new IllegalArgumentException("Pick a valid start");
+        }
+        if(start == destination){
+            throw new IllegalArgumentException("Start and destination floors are the same");
+        }
+        floorQueue.add(destination);
+        setDirection();
+        
+
+       
+    }
+
+    public void setDirection(){
+        if(floorQueue.isEmpty()){
+            direction = Direction.IDLE;
+        }
+        if(floorQueue.get(0) > currentFloor && direction == Direction.IDLE){
+            direction = Direction.UP;
+        }
+        if(floorQueue.get(0) < currentFloor && direction == Direction.IDLE){
+            direction = Direction.DOWN;
+        }
+
 
 
 }
+
+public void move(){
+    if(direction == Direction.UP){
+}   
+
+
+
+
+}}
 
 
 
